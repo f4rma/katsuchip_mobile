01a0f744a4b5eadaacfac36277a9cf8ef80c434b
Nama Anda <radityapfarma15@gmail.com>
2025-12-04 03:48:10 +0700

    Katsuchip

--- lib/config/maps_config.dart
+++
@@ -1,5 +0,0 @@
-class MapsConfig {
-  // Isi dengan API key Google Maps/Geocoding Anda.
-  // Biarkan kosong ('') untuk menonaktifkan dan gunakan fallback Nominatim (OSM).
-  static const String googleMapsApiKey = 'AIzaSyA8bmGehbS4aFRuC8TgZnYdGA_qBOZuyJs';
-}
